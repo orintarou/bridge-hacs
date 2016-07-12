51dfb4a958ca6740a936a23c62a7d48fca5e3bb2
Kumaran Chanthrakumar <kumaranc46@gmail.com>
2016-07-12 03:52:16 -0400

    Added description

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,11 @@
 var app = angular.module("BridgeApp", [])
+var databaseLink = new Firebase('https://senpai-notice-me.firebaseio.com/');  
 
 app.controller("BridgeController", function ($scope) {
+
+	$scope.todoAdd = function() {
+        alert(name);
+    };
+
 	$scope.hello = "Hello from the other side";
 });
