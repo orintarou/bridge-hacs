44c65f83a4b1d1caec69ed9cabc3e2c067a7e7eb
Kumaran Chanthrakumar <kumaranc46@gmail.com>
2016-07-12 05:42:22 -0400

    Finished database link

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,18 @@
 var app = angular.module("BridgeApp", [])
-var databaseLink = new Firebase('https://senpai-notice-me.firebaseio.com/');  
+var databaseLink = new Firebase('https://bridgehacs.firebaseio.com/');  
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
 
 app.controller("BridgeController", function ($scope) {
-
-	$scope.todoAdd = function() {
-        alert(name);
-    };
-
-	$scope.hello = "Hello from the other side";
+	$scope.todoAdd = function(name, email) {
+		if(validateEmail(email)){
+			databaseLink.push({name:name, email:email});
+			{window.location.reload();}
+		}else{
+			alert("Not a valid email? If it is...email us!");
+		};
+	};
 });
