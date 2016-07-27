2d3259a92d36b9b55842b211f5bf8e5783334dad
Kumaran Chanthrakumar <kumaranc46@gmail.com>
2016-07-27 18:29:05 -0400

    updated app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,10 @@
 var app = angular.module("BridgeApp", []);
-var databaseLink = new Firebase('https://bridgehacs.firebaseio.com/');  
+var databaseLink = new Firebase('https://bridgehacs.firebaseio.com/');
 
 function validateEmail(email) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
-
-// function validateBirthday(birthday){
-// 	var re = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
-// 	return re.test(birthday);
-// }
 
 function show(answer){
 	document.getElementById(answer + 'Questions').style.display = "block";
@@ -21,17 +16,14 @@ function show(answer){
 }
 
 app.controller("BridgeController", function ($scope) {
-	$scope.RSVP = function(){
-		{window.location.href = '/rsvp.html'}
-	};
-
+  
 	$scope.addUser = function(firstName, lastName, email, birthday, location,
-	 college="", gradYear="", major="", 
+	 college="", gradYear="", major="",
 	 nonGrad="", progExp, fact=""){
 	 	if(validateEmail(email)){
 	 		databaseLink.push({firstName:firstName, lastName:lastName,
 	 			email:email, birthday:birthday, location:location, college:college,
-	 			gradYear:gradYear, major:major, nonGrad:nonGrad, 
+	 			gradYear:gradYear, major:major, nonGrad:nonGrad,
 	 			progExp:progExp, fact:fact});
 	 		{window.location.href = '/thanks.html'}
 	 	}else{
@@ -40,10 +32,6 @@ app.controller("BridgeController", function ($scope) {
 	 		}
 	 	}
 	 };
-
-	$scope.changeForm = function() {
-		alert("yoo");
-	};
 
 	$scope.todoAdd = function(name, email) {
 		if(validateEmail(email)){
