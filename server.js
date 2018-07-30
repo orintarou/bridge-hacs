00c13714e2965f10dfefdd36e61377f83c2e0735
Kumaran Chanthrakumar <kumaranc46@gmail.com>
2018-07-30 16:24:32 -0400

    Moving to heroku

--- server.js
+++ server.js
@@ -3,7 +3,4 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
-app.listen(port,ip);
+app.listen(3000, '127.0.0.1');
