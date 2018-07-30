94d1cae5f96b99230094195b17e7e5598ca3e311
Kumaran Chanthrakumar <kumaranc46@gmail.com>
2018-07-30 16:34:54 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.use(express.static(__dirname + '/public'));
-
-app.listen(3000, '127.0.0.1');
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
